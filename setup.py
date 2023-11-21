726688aed7cdd603e06b1584c80a123888735787
ShintaroMinami <sminami@Monods-MacBook-Pro.local>
2023-11-20 16:01:25 -0800

    fix requirements

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
         'numpy',
         'pandas',
         'scipy',
-        'tensorflow-cpu',
+        'tensorflow',
     ],
     tests_require=[
         'matplotlib',  # For notebook_utils_test.
